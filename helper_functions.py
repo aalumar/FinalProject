ea71a47e8d6789fb4183a9692173158255488c0d
aalumar <81359182+aalumar@users.noreply.github.com>
2023-04-05 01:49:28 +0300

    Add files via upload

---
+++ helper_functions.py
@@ -0,0 +1,37 @@
+from typing import Dict, List
+import matplotlib.pyplot as plt
+
+def plot_loss_curves(results: Dict[str, List[float]]):
+  """
+  Plots a training curve of a results dictionary
+  """
+
+  # get loss values of dictionary
+  loss = results["train_loss"]
+  test_loss = results["test_loss"]
+
+  # get accuracy values of dictionary
+  accuracy = results["train_acc"]
+  test_accuracy = results["test_acc"]
+
+  # how many epochs
+  epochs = range(len(results["train_loss"]))
+
+  # set up plot
+  plt.figure(figsize=(15, 7))
+
+  # plot loss
+  plt.subplot(1, 2, 1)
+  plt.plot(epochs, loss, label="train_loss")
+  plt.plot(epochs, test_loss, label="test_loss")
+  plt.title("Loss")
+  plt.xlabel("Epochs")
+  plt.legend()
+
+  # plot accuracy
+  plt.subplot(1, 2, 2)
+  plt.plot(epochs, accuracy, label="train_accuracy")
+  plt.plot(epochs, test_accuracy, label="test_accuracy")
+  plt.title("Accuracy")
+  plt.xlabel("Epochs")
+  plt.legend()
